46ebb96b22c1f3470b43578635c3c0e432da0fa4
Riccardo Coppola <ricca509@gmail.com>
2020-08-09 22:53:31 +0100

    Adds logic to match system behaviour before refactoring

--- js/spec/gilded_rose.spec.js
+++ js/spec/gilded_rose.spec.js
@@ -1,74 +1,74 @@
 const { inn, Item } = require('../src/gilded_rose');
 
 describe("Gilded Rose", function () {
-  describe("updateQuality", () => {
+  describe("updateItem", () => {
     it('should decrease the number of days we have to sell the item by 1', () => {
       const gildedRose = inn([new Item("Item", 10, 20)]);
 
-      expect(gildedRose.updateQuality()[0].sell_in).toEqual(9);
-      expect(gildedRose.updateQuality()[0].sell_in).toEqual(8);
-      expect(gildedRose.updateQuality()[0].sell_in).toEqual(7);
+      expect(gildedRose.updateItem()[0].sell_in).toEqual(9);
+      expect(gildedRose.updateItem()[0].sell_in).toEqual(8);
+      expect(gildedRose.updateItem()[0].sell_in).toEqual(7);
     });
 
     it("should degrade every day by 1", () => {
       const gildedRose = inn([new Item("Item", 5, 20)]);
 
-      expect(gildedRose.updateQuality()[0].quality).toEqual(19);
-      expect(gildedRose.updateQuality()[0].quality).toEqual(18);
-      expect(gildedRose.updateQuality()[0].quality).toEqual(17);
+      expect(gildedRose.updateItem()[0].quality).toEqual(19);
+      expect(gildedRose.updateItem()[0].quality).toEqual(18);
+      expect(gildedRose.updateItem()[0].quality).toEqual(17);
     });
 
     it("should degrade twice as fast once the sell by date has passed", () => {
       const gildedRose = inn([new Item("Item", 1, 20)]);
 
-      expect(gildedRose.updateQuality()[0].quality).toEqual(19);
-      expect(gildedRose.updateQuality()[0].quality).toEqual(17);
-      expect(gildedRose.updateQuality()[0].quality).toEqual(15);
+      expect(gildedRose.updateItem()[0].quality).toEqual(19);
+      expect(gildedRose.updateItem()[0].quality).toEqual(17);
+      expect(gildedRose.updateItem()[0].quality).toEqual(15);
     });
 
     it("should never be negative", () => {
       let gildedRose = inn([new Item("Item", 5, 1)]);
 
-      expect(gildedRose.updateQuality()[0].quality).toEqual(0);
-      expect(gildedRose.updateQuality()[0].quality).toEqual(0);
+      expect(gildedRose.updateItem()[0].quality).toEqual(0);
+      expect(gildedRose.updateItem()[0].quality).toEqual(0);
       
       gildedRose = inn([new Item("Item", 5, -1)]);
-      expect(gildedRose.updateQuality()[0].quality).toEqual(0);
+      expect(gildedRose.updateItem()[0].quality).toEqual(0);
     });
 
     describe("Aged Brie", () => {
       it("should increase in Quality (+1) the older it gets", () => {
         const gildedRose = inn([new Item("Aged Brie", 5, 2)]);
 
-        expect(gildedRose.updateQuality()[0].quality).toEqual(3);
-        expect(gildedRose.updateQuality()[0].quality).toEqual(4);
-        expect(gildedRose.updateQuality()[0].quality).toEqual(5);
+        expect(gildedRose.updateItem()[0].quality).toEqual(3);
+        expect(gildedRose.updateItem()[0].quality).toEqual(4);
+        expect(gildedRose.updateItem()[0].quality).toEqual(5);
       });
 
       it("should never be more than 50", () => {
         const gildedRose = inn([new Item("Aged Brie", 5, 49)]);
 
-        expect(gildedRose.updateQuality()[0].quality).toEqual(50);
-        expect(gildedRose.updateQuality()[0].quality).toEqual(50);
+        expect(gildedRose.updateItem()[0].quality).toEqual(50);
+        expect(gildedRose.updateItem()[0].quality).toEqual(50);
       });
     });
 
     describe("Sulfuras, Hand of Ragnaros", () => {
       it("should always have a quality of 80", () => {
         const gildedRose = inn([new Item("Sulfuras, Hand of Ragnaros", 5, 10)]);
-        expect(gildedRose.updateQuality()[0].quality).toEqual(80);
+        expect(gildedRose.updateItem()[0].quality).toEqual(80);
       });
 
       it("should never decrease in Quality", () => {
         const gildedRose = inn([new Item("Sulfuras, Hand of Ragnaros", 5, 80)]);
-        expect(gildedRose.updateQuality()[0].quality).toEqual(80);
-        expect(gildedRose.updateQuality()[0].quality).toEqual(80);
+        expect(gildedRose.updateItem()[0].quality).toEqual(80);
+        expect(gildedRose.updateItem()[0].quality).toEqual(80);
       });
 
       it("should never have to be sold", () => {
         const gildedRose = inn([new Item("Sulfuras, Hand of Ragnaros", 5, 80)]);
-        expect(gildedRose.updateQuality()[0].sell_in).toEqual(5);
-        expect(gildedRose.updateQuality()[0].sell_in).toEqual(5);
+        expect(gildedRose.updateItem()[0].sell_in).toEqual(5);
+        expect(gildedRose.updateItem()[0].sell_in).toEqual(5);
       });
     });
 
@@ -76,38 +76,38 @@ describe("Gilded Rose", function () {
       describe("increase in Quality the older they get", () => {
         it("should increase in Quality by 2 when there are 10 days or less", () => {
           const gildedRose = inn([
-            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 2),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 11, 2),
           ]);
 
-          expect(gildedRose.updateQuality()[0].quality).toEqual(4);
-          expect(gildedRose.updateQuality()[0].quality).toEqual(6);
-          expect(gildedRose.updateQuality()[0].quality).toEqual(8);
-          expect(gildedRose.updateQuality()[0].quality).toEqual(10);
-          expect(gildedRose.updateQuality()[0].quality).toEqual(12);
+          expect(gildedRose.updateItem()[0].quality).toEqual(4);
+          expect(gildedRose.updateItem()[0].quality).toEqual(6);
+          expect(gildedRose.updateItem()[0].quality).toEqual(8);
+          expect(gildedRose.updateItem()[0].quality).toEqual(10);
+          expect(gildedRose.updateItem()[0].quality).toEqual(12);
         });
 
         it("should increase in Quality by 3 when there are 5 days or less", () => {
           const gildedRose = inn([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 2)]);
 
-          expect(gildedRose.updateQuality()[0].quality).toEqual(5);
-          expect(gildedRose.updateQuality()[0].quality).toEqual(8);
-          expect(gildedRose.updateQuality()[0].quality).toEqual(11);
-          expect(gildedRose.updateQuality()[0].quality).toEqual(14);
-          expect(gildedRose.updateQuality()[0].quality).toEqual(17);
+          expect(gildedRose.updateItem()[0].quality).toEqual(5);
+          expect(gildedRose.updateItem()[0].quality).toEqual(8);
+          expect(gildedRose.updateItem()[0].quality).toEqual(11);
+          expect(gildedRose.updateItem()[0].quality).toEqual(14);
+          expect(gildedRose.updateItem()[0].quality).toEqual(17);
         });
       });
 
       it("should drop quality to 0 after the concert", () => {
         const gildedRose = inn([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]);
 
-        expect(gildedRose.updateQuality()[0].quality).toEqual(0);
+        expect(gildedRose.updateItem()[0].quality).toEqual(0);
       });
 
       it("should never be more than 50", () => {
         const gildedRose = inn([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]);
 
-        expect(gildedRose.updateQuality()[0].quality).toEqual(50);
-        expect(gildedRose.updateQuality()[0].quality).toEqual(50);
+        expect(gildedRose.updateItem()[0].quality).toEqual(50);
+        expect(gildedRose.updateItem()[0].quality).toEqual(50);
       });
     });
   });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -4,31 +4,101 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
+Item.prototype.updateSellIn = function () {
+  let updatedSellIn = this.sell_in - 1;
+
+  if (specialItems[this.name] && specialItems[this.name].updateSellIn) {
+    updatedSellIn = specialItems[this.name].updateSellIn({
+      name: this.name,
+      sell_in: this.sell_in,
+      quality: this.quality,
+    });
+  }
+
+  this.sell_in = updatedSellIn;
+
+  return this;
+};
+
+Item.prototype.updateQuality = function () {
+  const updatedQuality = calculateUpdatedQuality({
+    name: this.name,
+    sell_in: this.sell_in,
+    quality: this.quality,
+  });
+
+  this.quality = updatedQuality;
+
+  return this;
+};
+
+const specialItems = {
+  "Aged Brie": {
+    updateQuality: ({ quality }) => {
+      return quality < 50 ? increaseByOne(quality) : 50;
+    },
+  },
+  "Sulfuras, Hand of Ragnaros": {
+    updateQuality: () => {
+      return 80;
+    },
+    updateSellIn: ({ sell_in }) => {
+      return sell_in;
+    },
+  },
+  "Backstage passes to a TAFKAL80ETC concert": {
+    updateQuality: ({ quality, sell_in }) => {
+      if (sell_in < 0) {
+        return 0;
+      }
+      let updatedQuality = increaseByOne(quality);
+      
+      if (sell_in <= 10) {
+        updatedQuality = increaseByTwo(quality);
+      }
+
+      if (sell_in <= 5) {
+        updatedQuality = increaseByThree(quality);
+      }
+
+      return updatedQuality <= 50 ? updatedQuality : 50;
+    },
+  },
+};
+
+const isPastSellDate = (sellDate) => sellDate < 0;
+
+const add = (base) => (toNumber) => base + toNumber;
+const sub = (base) => (fromNumber) => fromNumber - base;
+const increaseByOne = add(1);
+const increaseByTwo = add(2);
+const increaseByThree = add(3);
+const decreaseByOne = sub(1);
+const decreaseByTwo = sub(2);
+
+const calculateUpdatedQuality = ({ name, sell_in, quality }) => {
+  if (quality === 0) return quality;
+
+  if (specialItems[name] && specialItems[name].updateQuality) {
+    return specialItems[name].updateQuality({ name, sell_in, quality });
+  }
+
+  if (isPastSellDate(sell_in)) {
+    return decreaseByTwo(quality);
+  }
+
+  return quality > 0 ? decreaseByOne(quality) : 0;
+};
+
 const inn = (items = []) => {
   let itemsToSell = [...items];
 
-  const isPastSellDate = (sellDate) => sellDate <= 0;
+  const updateItem = () => {
+    const newItems = itemsToSell.map((item) => {
+      item.updateSellIn();
+      item.updateQuality();
 
-  const calculateUpdatedQuality = ({ name, sell_in, quality }) => {
-    if (quality === 0) return quality;
-
-    if (isPastSellDate(sell_in)) {
-      return (quality - 2);
-    }
-
-    return quality > 0 ? (quality - 1) : 0;
-  };
-
-  const updateQuality = () => {
-    const newItems = itemsToSell.map(({ name, sell_in, quality }) => {
-      const updatedSellIn = sell_in > 0 ? (sell_in - 1) : sell_in;
-      const updatedQuality = calculateUpdatedQuality({ name, sell_in, quality });
-
-      return {
-        name,
-        sell_in: updatedSellIn,
-        quality: updatedQuality,
-      };
+      return item;
     });
 
     itemsToSell = [...newItems];
@@ -37,7 +107,7 @@ const inn = (items = []) => {
   };
 
   return {
-    updateQuality,
+    updateItem,
   };
 };
 
